2942c502dacaf3619c5caa08613985b7419268a6
Chad Katzen <katzen.chad@gmail.com>
2023-02-02 14:02:32 -0500

    starting new styles

--- src/components/HomeComponents/DescriptionTwo.js
+++ src/components/HomeComponents/DescriptionTwo.js
@@ -27,7 +27,7 @@ function DescriptionTwo() {
 
         <Box textAlign = "center" >
             <Box sx={{ flexGrow: 1,m:4}}>
-            <Typography variant="h2" style={{color:yellow}} >~Lotto~</Typography>
+            <Typography variant="h2" sx={{fontFamily: 'Clarkson'}} style={{color:yellow}} >~Lotto~</Typography>
             <br/>
          
 
@@ -38,8 +38,8 @@ function DescriptionTwo() {
                 <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                     <Box sx={{display:'flex', justifyContent: "space-around", flexWrap:"wrap-reverse"}} >
                         <Box  sx={{m:3}}>
-                            <Box className={classes.container}>
-                            <Typography variant="h6" style={{color:yellow}} textAlign ="left" sx={{m: 6}}>
+                            <Box sx={{borderRadius:'20px'}} className={classes.container}>
+                            <Typography variant="h6" style={{color:yellow}} textAlign ="left" sx={{fontFamily: 'Clarkson', m: 6}}>
                                 Your chance at up to 1,000 eth
                                 <br/><br/>
                                 Week by week, Lotto tickets will be eliminated until one lucky winner remains
@@ -50,10 +50,11 @@ function DescriptionTwo() {
                             </Typography>
                             </Box>
                         </Box>
-                        <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', m:3}}>
+                        <Box sx={{display: {xs: 'none', sm: 'none', md: 'none', lg: 'flex'}, alignItems: 'center', justifyContent: 'center', m:3}}>
                             <Box 
                             component="img"
                             sx={{
+                                
                                 ml: 0,
                                 mr:0,
                                 maxHeight: { xs: 350, sm: 400 , md: 400, lg: 300, xl: 400},
--- src/components/HomeComponents/HowItWorks.js
+++ src/components/HomeComponents/HowItWorks.js
@@ -10,52 +10,54 @@ function HowItWorks(){
         <Box textAlign="center">
             <Typography variant="h2" style={{color:yellow}} >~How It Works~</Typography>
           
+            
 
+                <Grid container justifyContent="center" alignItems="center" >
+                  <Grid item xs= {6} sm = {3} md = {3} lg={3}>
 
-               
-                <Box  sx={{display:'flex', justifyContent:'space-around'}}>
-                  
-                  <Box sx={{display:'flex', justifyContent:'space-evenly', alignItems: 'center', justifyContent: 'center'}}>
-                    <Box 
-                    component="img"
-                    sx={{
-                        ml: 0,
-                        mr:0,
-                        maxHeight:  350,
-                        maxWidth: 233,
-                        flexGrow: 1
-                    }}
-                    alt="Place Holder Lottery Ticket"
-                    src={ticketImage}
-                    />
-                    <Box 
-                    component="img"
-                    sx={{
-                        ml: 0,
-                        mr:0,
-                        maxHeight:  350,
-                        maxWidth: 233,
-                        flexGrow: 1
-                    }}
-                    alt="Place Holder Lottery Ticket"
-                    src={ticketImage}
-                    />
-                  </Box>
+                      <Box 
+                      component="img"
+                      sx={{
+                          m:4,
+                          maxHeight: { xs: 200, sm: 200 , md: 260, lg: 300, xl: 400},
+                          flexGrow: 1
+                      }}
+                      alt="Place Holder Lottery Ticket"
+                      src={ticketImage}
+                      />
+                  </Grid>
 
-                  <Box>
-                    <Typography variant="h6" style={{color:yellow}} textAlign ="justify" sx={{m: 6}}>
-                      Lotto kicks off with an initial mint of up to 10,000 Lotto tickets. 
-                      Then, every Saturday at midnight, a percentage of Lotto tickets will be eliminated from the prize pool and will no longer have a chance of taking home the jackpot.
-                      Ticket images will be updated to reflect eliminations, and the status of any ticket can always be checked on the check ticket tab. 
-                    </Typography>
-                  
-                    <Typography variant="h6" style={{color:yellow}} textAlign ="justify" sx={{m: 6}}>
-                      If your ticket is eliminated, it can be burned to guarantee whitelist access to the next Lotto drop. 
-                      See below for a full schedule of eliminations.
-                    </Typography>
-                  </Box>
+                  <Grid item xs= {6} sm= {3} md = {3} lg ={3}>
+                      <Box 
+                      component="img"
+                      sx={{
+                          m:4,
+                          maxHeight: { xs: 200, sm: 200 , md: 260, lg: 300, xl: 400},
+                          flexGrow: 1
+                      }}
+                      alt="Place Holder Lottery Ticket"
+                      src={ticketImage}
+                      />
 
-                </Box>
+                  </Grid>
+                    
+                  <Grid item xs = {12} sm ={6} md = {6} lg ={6}>
+                    <Box>
+                      <Typography variant="h6" style={{color:yellow}} textAlign ="justify" sx={{m: 6}}>
+                        Lotto kicks off with an initial mint of up to 10,000 Lotto tickets. 
+                        Then, every Saturday at midnight, a percentage of Lotto tickets will be eliminated from the prize pool and will no longer have a chance of taking home the jackpot.
+                        Ticket images will be updated to reflect eliminations, and the status of any ticket can always be checked on the check ticket tab. 
+                      </Typography>
+                    
+                      <Typography variant="h6" style={{color:yellow}} textAlign ="justify" sx={{m: 6}}>
+                        If your ticket is eliminated, it can be burned to guarantee whitelist access to the next Lotto drop. 
+                        See below for a full schedule of eliminations.
+                      </Typography>
+                    </Box>
+                  </Grid>
+
+                </Grid>
+                
 
                   
 
